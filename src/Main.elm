89bbf982ccc7fba0583810bc6e3e797b5ceb21aa
Garrett van Wageningen <ghvanw283@gmail.com>
2021-10-04 18:46:20 -0500

    save

--- src/Main.elm
+++ src/Main.elm
@@ -2,30 +2,43 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, button, div, text)
+import Html.Attributes exposing (datetime)
 import Html.Events exposing (onClick)
+import Time
+
 
 main : Program () Counter Msg
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.sandbox { init = 0, update = update, view = view }
 
-type alias Counter = Int
 
-type Msg = Increment | Decrement
+type alias Counter =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
 
 update : Msg -> Counter -> Counter
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            model + 1
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            model - 1
 
-view : Counter -> (Html Msg)
+
+view : Counter -> Html Msg
 view model =
-  div []
-    [ 
-        button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
+
+
+type alias Check =
+    { amount : Float, checkNumber : Int, payTo : String, date : Time.Posix }
